7b14f42a0b7587dc44a6fed501c8d4edbd8923f8
shiftay <shroebuck@gmail.com>
2018-02-08 18:57:23 -0500

    Change to gitignore, Basic State Machine implementation

---
+++ Assets/Scripts/States/Loading.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loading : MonoBehaviour {
+
+	public float timer = 0f;
+	
+	/// <summary>
+	/// This function is called when the object becomes enabled and active.
+	/// </summary>
+	void OnEnable()
+	{
+		timer = 0;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+
+		if(timer > 5) {
+			StateManager.instance.ChangeState(GameStates.Menu);
+		}
+	}
+}
---
+++ Assets/Scripts/States/StateManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameStates { Loading, Menu, Main, Play, Pause, Settings }
+
+
+public class StateManager : MonoBehaviour {
+
+	List<GameObject> states = new List<GameObject>();
+	public GameStates currentState;
+	public GameStates previousState;
+	public static StateManager instance; 
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+		for(int i = 0; i < transform.childCount; i++) {
+			states.Add(transform.GetChild(i).gameObject);
+		}
+
+		ChangeState(GameStates.Loading);
+	}
+
+	public void ChangeState(GameStates test) {
+		previousState = currentState;
+		states[(int)currentState].SetActive(false);
+		currentState = test;
+		states[(int)currentState].SetActive(true);
+	}
+
+}
