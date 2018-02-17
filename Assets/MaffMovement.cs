5ee760c00c1f24c3582422923df28b08295fa01e
shiftay <shroebuck@gmail.com>
2018-02-16 20:28:30 -0500

    Quickmaff, W/L completed.

---
+++ Assets/MaffMovement.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaffMovement : MonoBehaviour {
+
+ public bool draggingItem = false;
+    private GameObject draggedObject;
+    private Vector2 touchOffset;
+	public bool reorderPuz = false;
+	public Vector2 droppedPos;
+	public bool gameOver = false;
+
+
+    void Update ()
+    {
+		if(!gameOver) {
+			if (HasInput){
+				DragOrPickUp();
+			} else {
+				if (draggingItem)
+					DropItem();
+			}
+		}
+    }
+     
+    Vector2 CurrentTouchPosition
+    {
+        get
+        {
+            Vector2 inputPos;
+            inputPos = Input.mousePosition;
+            return inputPos;
+        }
+    }
+ 
+    private void DragOrPickUp()
+    {
+        var inputPosition = CurrentTouchPosition;
+     
+        if (draggingItem)
+        {
+            draggedObject.transform.position = inputPosition + touchOffset;
+        }
+        else
+        {
+            RaycastHit2D[] touches = Physics2D.RaycastAll(inputPosition, inputPosition, 0.5f);
+            if (touches.Length > 0)
+            {
+                var hit = touches[0];
+                if (hit.transform != null) {
+                    if(hit.transform.tag == "Sign_MAFF") {
+                        draggingItem = true;
+                        draggedObject = hit.transform.gameObject;
+                        touchOffset = (Vector2)hit.transform.position - inputPosition;
+                        // draggedObject.transform.localScale = new Vector3(1.2f,1.2f,1.2f);
+                    }
+                }
+            }
+        }
+    }
+ 
+    private bool HasInput
+    {
+        get
+        {
+            // returns true if either the mouse button is down or at least one touch is felt on the screen
+            return Input.GetMouseButton(0);
+        }
+    }
+ 
+    void DropItem()
+    {
+        draggingItem = false;
+        draggedObject.transform.localScale = new Vector3(1f,1f,1f);
+		reorderPuz = true;
+		droppedPos = CurrentTouchPosition;
+    }
+
+}
---
+++ Assets/QuickMaff.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum MAFFSIGNS { ADD, SUBTRACT, MULTIPLY, DIVIDE };
+
+public class QuickMaff : MonoBehaviour {
+
+	public Text answerT;
+	public Text firstSide;
+	public Text secondSide;
+	public MAFFSIGNS answerKey;
+	public int[] primeNumber = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+ 	public GameObject[] signStarts;
+	public Vector2[] startPositions;
+	MaffMovement maff;
+	public GameObject SIGN_ANSWER;
+
+	bool gameOver = false;
+
+
+	// Use this for initialization
+	void Start () {
+
+		maff = GetComponent<MaffMovement>();
+		startPositions = new Vector2[signStarts.Length];
+
+		for(int i = 0; i < signStarts.Length; i++) {
+			startPositions[i] = signStarts[i].transform.position;
+		}
+
+		
+
+		bool canFindAnswer = false;
+		int actualAnswer = -1;
+		int leftside_a = -1;
+		int rightside_a = 1; 
+
+		answerKey = (MAFFSIGNS)Random.Range(0,4);
+
+			int answer = -1;
+
+			switch(answerKey) {
+				case MAFFSIGNS.ADD:
+					answer = Random.Range(0,100);
+					leftside_a = Random.Range(1, answer);
+					rightside_a = answer - leftside_a;
+					actualAnswer = answer;
+					canFindAnswer = true;
+					break;
+				case MAFFSIGNS.SUBTRACT:
+					answer = Random.Range(0,50);
+					leftside_a = Random.Range(answer+1, answer * 2);
+					rightside_a = leftside_a - answer;
+					actualAnswer = answer;
+					canFindAnswer = true;
+					break;
+				case MAFFSIGNS.DIVIDE:
+					answer = Random.Range(11,25);
+					rightside_a = Random.Range(0, 10);
+					leftside_a = answer * rightside_a;
+					actualAnswer = answer;
+					canFindAnswer = true;
+
+
+
+
+					break;
+				case MAFFSIGNS.MULTIPLY:
+					do{
+						answer = Random.Range(0,100);
+					} while(CalcIsPrime(answer));
+					
+					do {
+						leftside_a = Random.Range(2,10);
+					} while(answer % leftside_a != 0);
+
+						rightside_a = answer / leftside_a;
+
+					break;
+			}
+
+
+		answerT.text = answer.ToString();
+		secondSide.text = rightside_a.ToString();
+		firstSide.text = leftside_a.ToString();
+
+	}
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		if(!gameOver) {
+			if(maff.reorderPuz) {
+				if(realign()) {
+					// TODO: CHECK IF THEY WIN OR NOT.
+					DidWeWin();
+				} 
+			}	
+		}
+	}
+
+
+	void DidWeWin() {
+		int outOfPosition = -1;
+
+		for(int i = 0; i < signStarts.Length; i++) {
+			if(!V2Equal(signStarts[i].transform.position, startPositions[i])) {
+				outOfPosition = i;
+			}
+		}
+
+		if(answerKey == (MAFFSIGNS)outOfPosition){
+			Debug.Log("YOU WON");
+		} else {
+			Debug.Log("YOU lost");
+		}
+
+	}
+
+
+	bool realign() {
+		bool retVal = false;
+		int outOfPosition = -1;
+
+		for(int i = 0; i < signStarts.Length; i++) {
+			if(!V2Equal(signStarts[i].transform.position, startPositions[i])) {
+				outOfPosition = i;
+			}
+		}
+
+
+		if(V2EqualB(signStarts[outOfPosition].transform.position, SIGN_ANSWER.transform.position)) {
+			signStarts[outOfPosition].transform.position = SIGN_ANSWER.transform.position;
+			maff.gameOver = true;
+			gameOver = true;
+			retVal = true;
+		} else {
+			signStarts[outOfPosition].transform.position = startPositions[outOfPosition];
+		}
+		maff.reorderPuz = false;
+		return retVal;
+	}
+
+	bool V2Equal(Vector2 a, Vector2 b) {
+		return Vector2.SqrMagnitude(a - b) < 0.0001;
+	}
+
+	
+	bool V2EqualB(Vector2 a, Vector2 b) {
+		return Mathf.Abs(a.magnitude - b.magnitude) < 10;
+	}
+
+
+	public bool CalcIsPrime(int number) {
+	
+		if (number == 1) return false;
+		if (number == 2) return true;
+	
+		if (number % 2 == 0) return false; // Even number     
+	
+		for (int i = 2; i < number; i++) { // Advance from two to include correct calculation for '4'
+			if (number % i == 0) return false;
+		}
+	
+		return true;
+	
+	}
+ }
